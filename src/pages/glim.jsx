e617a5a20176447c6a91c567596f90b2733b9b3b
fitodac <fitodac@gmail.com>
2023-07-20 18:25:17 -0300

    Glim form update

--- src/pages/glim.jsx
+++ src/pages/glim.jsx
@@ -3,34 +3,182 @@ import gsap from 'gsap'
 import Link from 'next/link'
 
 import Layout from '../layouts/Layout'
-import List from '../components/List'
-import Results from '../components/Results'
+import Results from '../components/ResultsGlim'
 
-const form_value = {option1: null, option2: null, option3: null }
+const form_value = {
+	tall: '1,55',
+	weight: '50',
+	usual_weight: '58,3',
+	// tall: '',
+	// weight: '',
+	// usual_weight: '',
+	more_than_6_months: 0,
+	loss_weight: '00,00',
+	loss_weight_percent: 0,
+	imc: '00,00',
+
+	age: 0,
+	// muscle_mass: '',
+	// reduced_dietary_intake: '',
+	// inflammation: '',
+	
+	reduced_dietary_intake: '&lt;50% de las necesidades energéticas &gt;1 semana',
+	muscle_mass: 'Déficit leve a moderado',
+	inflammation: 'Relacionada a enfermedad/lesión aguda',
+	
+	loss_weight_info: '',
+	low_imc: ''
+}
+
+
+const calcSixMonths = val => val ? 'más de 6 meses' : 'menos de 6 meses'
+
+
+const calculator = form => {
+	const tall = parseFloat(form.tall.replace(',','.'))
+	const weight = parseFloat(form.weight.replace(',','.'))
+	const usual_weight = parseFloat(form.usual_weight.replace(',','.'))
+	const imc = parseFloat(form.imc.replace(',','.'))
+	const f = {...form}
+	
+	if( weight && usual_weight ){
+		const result = usual_weight - weight
+		f.loss_weight = result <= 0 ? 0 : result.toFixed(2).replace('.',',')
+		f.loss_weight_percent = (parseFloat(f.loss_weight.replace(',','.')) / usual_weight) * 100
+
+		if( f.loss_weight_percent <= 10 && f.loss_weight_percent > 5 ) f.loss_weight_info = `5-10% en ${calcSixMonths(f.more_than_6_months)}`
+		if( f.loss_weight_percent == 10 ) f.loss_weight_info = `10% en ${calcSixMonths(f.more_than_6_months)}`
+		if( f.loss_weight_percent > 10 && f.loss_weight_percent < 20 ) f.loss_weight_info = `10-20% ${calcSixMonths(f.more_than_6_months)}`
+		if( f.loss_weight_percent >= 20 ) f.loss_weight_info = `20% en ${calcSixMonths(f.more_than_6_months)}`
+	}
+	
+	if( weight && tall ){
+		f.imc = (weight / Math.pow(tall, 2)).toFixed(2).replace('.',',')
+	}
+
+
+	if( imc < 22 ){
+		if( imc < 20 ){
+			if( form.age ){
+				f.low_imc = '< 20 en > 70 años'
+			}else{
+				if( imc < 18.5 ){
+					if( !form.age ){}
+					f.low_imc = '< 18,5 en < 70 años'
+				}else{
+					if( !form.age ){}
+				}
+			}
+		}else{
+			if( form.age ) f.low_imc = '< 22 en > 70 años'
+		}
+	}
+
+	console.log('f', f)
+
+	return f
+}
+
 
 export default function GlimPage(){
 
 	const [form, setForm] = useState({...form_value})
 	const [sent, setSent] = useState(false)
 	const [error, setError] = useState(false)
-	const [malnutrition, setMalnutrition] = useState(null)
+	const [diagnosis, setDiagnosis] = useState(null)
 	const [breadcrumb, setBreadcrumb] = useState('glim')
 	const [step, setStep] = useState(1)
 
 	useEffect(() => {
-
+		setForm(calculator(form))
 	}, [])
 
+	useEffect(() => setForm(calculator(form)), [form.tall, form.weight, form.usual_weight, form.more_than_6_months, form.age])
+
+
+	const showError = () => {
+		setError(true)
+		setTimeout(() => {
+			gsap.to('#errorBox', { 
+				opacity: 1, 
+				onComplete: () => setTimeout(() => {
+					gsap.to('#errorBox', { opacity: 0, onComplete: () => setError(false) })
+				}, 2000)
+			})
+		}, 100)
+	}
+
+
 	const handleChange = e => {
+		const regex = /^[\d,.]+$/
 		const f = {...form}
-		f[e.target.name] = parseInt(e.target.value)
+
+		const forbidenCharacter = () => {
+			e.target.value = e.target.value.slice(0,-1)
+			return
+		}
+
+		if( isNaN( parseFloat(e.target.value) ) ){
+			f[e.target.name] = e.target.value
+			console.log('f', f)
+		}else{
+			if( regex.test(e.target.value) ){
+				if( e.target.value[0] === ',' || e.target.value[0] === '.' ) return forbidenCharacter()
+
+				if( '.' === e.target.value[e.target.value.length - 1] ){
+					e.target.value = e.target.value.slice(0,-1)
+					e.target.value += ','
+				}
+
+				const countPeriods = e.target.value.split(',')
+				if( countPeriods.length > 2 ) return forbidenCharacter()
+
+				f[e.target.name] = e.target.value
+			}else{
+				e.target.value = e.target.value.slice(0,-1)
+			}
+		}
+
 		setForm(f)
 	}
 
-	const formReset = () => setForm({...form_value})
+	const setTimeFrame = val => {
+		if( form.more_than_6_months === val ) return
+		const f = {...form}
+		f.more_than_6_months = val
+		setForm(f)
+	}
+
+	const setAge = val => {
+		if( form.age === val ) return
+		const f = {...form}
+		f.age = val
+		setForm(f)
+	}
+
+	const validateStep1 = () => {
+		console.log('validate')
+		if( !form.tall.length || !form.weight.length || !form.usual_weight.length ){
+			showError()
+			return
+		}
+		setStep(2)
+	}
+
 
 	const submit = e => {
 		e.preventDefault()
+
+		if( !form.muscle_mass.length || !form.reduced_dietary_intake.length || !form.inflammation.length ){
+			showError()
+			return
+		}
+
+		( 'No aplica' === form.reduced_dietary_intake && 'No aplica' == form.inflammation ) ? setDiagnosis('NO') : setDiagnosis('SÍ')
+		setBreadcrumb('result')
+		setSent(true)
+
+		console.log('FORM:', form)
 	}
 
 
@@ -44,54 +192,69 @@ export default function GlimPage(){
 							<div id="pageTitle" className="page-title">DATOS ANTROPOMÉTRICOS</div>
 						</div>
 
-						<div id="introText" className="text-center">Por favor, indique los siguientes datos de su paciente:</div>
-
-						
-						<section className="space-y-3 max-w-sm mx-auto">
+						<section className="space-y-3 max-w-md mx-auto">
+							<div className="text-center">Por favor, indique los siguientes datos de su paciente:</div>
 
 							<label className="bg-brand-aqua-300 border-b-2 border-brand-dark text-brand-aqua-600 flex items-center">
-								<span className="pl-4 py-2">Talla (m):</span>
+								<span className="pl-4 py-2 w-40">Talla (m):</span>
 								<input 
 									type="text" 
-									className="bg-transparent flex-1 px-4 py-2 focus:outline-none" />
+									className="bg-transparent text-right flex-1 px-4 py-2 focus:outline-none"
+									name="tall"
+									onChange={handleChange}
+									defaultValue={form.tall} />
 							</label>
 
 							<label className="bg-brand-aqua-300 border-b-2 border-brand-dark text-brand-aqua-600 flex items-center">
-								<span className="pl-4 py-2">Peso actual (kg):</span>
+								<span className="pl-4 py-2 w-40">Peso actual (kg):</span>
 								<input 
 									type="text" 
-									className="bg-transparent flex-1 px-4 py-2 focus:outline-none" />
+									className="bg-transparent text-right flex-1 px-4 py-2 focus:outline-none"
+									name="weight"
+									onChange={handleChange}
+									defaultValue={form.weight} />
 							</label>
 
 							<label className="bg-brand-aqua-300 border-b-2 border-brand-dark text-brand-aqua-600 flex items-center">
-								<span className="pl-4 py-2">Peso habitual (kg):</span>
+								<span className="pl-4 py-2 w-40">Peso habitual (kg):</span>
 								<input 
 									type="text" 
-									className="bg-transparent flex-1 px-4 py-2 focus:outline-none" />
+									className="bg-transparent text-right flex-1 px-4 py-2 focus:outline-none"
+									name="usual_weight"
+									onChange={handleChange}
+									defaultValue={form.usual_weight} />
 							</label>
 
 						</section>
 
-						<section className="mt-8 space-y-2 max-w-sm mx-auto">
+						<section className="mt-8 space-y-2 max-w-md mx-auto">
 							<div className="text-center">Cálculo peso perdido (kg):</div>
-							<div className="bg-brand-aqua border-b-4 border-brand-dark text-2xl font-semibold text-center p-2">00,00 kg</div>
+							<div className="bg-brand-aqua border-b-4 border-brand-dark text-2xl font-semibold text-center p-2">{form.loss_weight} kg</div>
 						</section>
 
-						<div className="mt-4 flex gap-x-8 max-w-sm mx-auto">
-							<button className="bg-white border border-stone-300 p-2 flex-1">&lt; 6 MESES</button>
-							<button className="bg-white border border-stone-300 p-2 flex-1">&gt; 6 MESES</button>
+						<div className="mt-4 flex gap-x-8 max-w-md mx-auto">
+							<button 
+								className={`${form.more_than_6_months ? 'bg-white' : 'bg-gray-300'} border border-gray-300 p-2 flex-1`}
+								onClick={() => setTimeFrame(0)}>
+								&lt; 6 MESES
+							</button>
+							<button 
+								className={`${form.more_than_6_months ? 'bg-gray-300' : 'bg-white'} border border-stone-300 p-2 flex-1`}
+								onClick={() => setTimeFrame(1)}>
+								&gt; 6 MESES
+							</button>
 						</div>
 
-						<section className="mt-8 space-y-2 max-w-sm mx-auto">
+						<section className="mt-8 space-y-2 max-w-md mx-auto">
 							<div className="text-center">Cálculo de IMC actual (kg/m<sup className="text-xxs">2</sup>):</div>
-							<div className="bg-brand-aqua border-b-4 border-brand-dark text-2xl font-semibold text-center p-2">00,00 kg</div>
+							<div className="bg-brand-aqua border-b-4 border-brand-dark text-2xl font-semibold text-center p-2">{form.imc} kg/m<sup className="text-xs">2</sup></div>
 						</section>
 
 						<div id="nextButton" className="pt-14 flex justify-center">
 							<button 
 								type="button"
 								className="bg-brand-aqua text-white font-bold px-9 py-2.5 transition-all hover:opacity-80"
-								onClick={() => setStep(2)}>
+								onClick={() => validateStep1()}>
 								CONTINUAR
 							</button>
 						</div>
@@ -102,67 +265,89 @@ export default function GlimPage(){
 							<div className="page-title">CRITERIOS GLIM PARA EL DIAGNÓSTICO DE DESNUTRICIÓN*</div>
 						</div>
 
-						<div className="text-center max-w-sm mx-auto">*Diagnóstico de desnutrición: requiere al menos 1 criterio fenotípico y 1 criterio etiológico.</div>
+						<div className="text-center max-w-md mx-auto">*Diagnóstico de desnutrición: requiere al menos <br/>1 criterio fenotípico y 1 criterio etiológico.</div>
 
-						<fieldset>
+						<fieldset className="max-w-md mx-auto">
 							<legend className="text-center">
-								<div className="bg-brand-aqua w-24 h-1 mx-auto mt-10"></div>
-								<div className="page-title text-brand-aqua mt-5">CRITERIO FENOTÍPICO</div>
+								<div className="bg-brand-aqua-600 w-24 h-1 mx-auto mt-10"></div>
+								<div className="page-title text-brand-aqua-600 mt-5">CRITERIO FENOTÍPICO</div>
 							</legend>
 
 							<section className="mt-7 space-y-6">
-								<div className="text-brand-aqua font-medium">Por favor, responda las siguientes preguntas sobre su paciente:</div>
+								<div className="text-brand-aqua-600 font-medium leading-tight">Por favor, responda las siguientes preguntas sobre su paciente:</div>
 
 								<div className="space-y-2">
 									<div className="">Pérdida de peso (%):</div>
-									<div className="bg-brand-aqua font-medium px-3 py-1.5">5-10% en los últimos 6 meses</div>
+									<div className="bg-brand-aqua font-medium px-3 py-1.5">{form.loss_weight_info}</div>
 								</div>
 
 								<div className="space-y-2">
 									<div className="">Bajo IMC (kg/m<sup className="text-xxs">2</sup>):</div>
-									<div className="bg-brand-aqua font-medium px-3 py-1.5">&lt; 20 en &lt; 70 años</div>
+									<div className="bg-brand-aqua font-medium px-3 py-1.5">{form.low_imc} &lt; 20 en &lt; 70 años</div>
 								</div>
 
 								<div className="space-y-2">
 									<div className="">Masa muscular reducida:</div>
-									<select name="" id="" className="border border-brand-aqua w-full px-2 py-1 focus:outline-none">
+									<select 
+										name="muscle_mass" 
+										onChange={handleChange}
+										className="border border-brand-aqua w-full px-2 py-1 focus:outline-none">
 										<option value="">Selecciona</option>
-										<option value="">Déficit leve a moderado</option>
-										<option value="">Déficit severo</option>
-										<option value="">No aplica</option>
+										<option value="Déficit leve a moderado">Déficit leve a moderado</option>
+										<option value="Déficit severo">Déficit severo</option>
+										<option value="No aplica">No aplica</option>
 									</select>
+								</div>
+
+								<div className="mt-4 flex gap-x-8 max-w-md mx-auto">
+									<button 
+										className={`${form.age ? 'bg-white' : 'bg-gray-300'} border border-gray-300 p-2 flex-1`}
+										onClick={() => setAge(0)}>
+										&lt; 70 AÑOS
+									</button>
+									<button 
+										className={`${form.age ? 'bg-gray-300' : 'bg-white'} border border-stone-300 p-2 flex-1`}
+										onClick={() => setAge(1)}>
+										&gt; 70 AÑOS
+									</button>
 								</div>
 							</section>
 						</fieldset>
 
 
-						<fieldset>
+						<fieldset className="max-w-md mx-auto">
 							<legend className="text-center pt-5">
-								<div className="bg-brand-aqua w-24 h-1 mx-auto mt-10"></div>
-								<div className="page-title text-brand-aqua mt-5">CRITERIO ETIOLÓGICO</div>
+								<div className="bg-brand-aqua-600 w-24 h-1 mx-auto mt-10"></div>
+								<div className="page-title text-brand-aqua-600 mt-5">CRITERIO ETIOLÓGICO</div>
 							</legend>
 
 							<section className="mt-7 space-y-6">
-								<div className="text-brand-aqua font-medium">Por favor, responda las siguientes preguntas sobre su paciente:</div>
+								<div className="text-brand-aqua-600 font-medium leading-tight">Por favor, responda las siguientes preguntas sobre su paciente:</div>
 
 								<div className="space-y-2">
 									<div className="">Reducción de la ingesta o asimilación de alimentos:</div>
-									<select name="" id="" className="border border-brand-aqua w-full px-2 py-1 focus:outline-none">
+									<select 
+										name="reduced_dietary_intake" 
+										onChange={handleChange} 
+										className="border border-brand-aqua w-full px-2 py-1 focus:outline-none">
 										<option value="">Selecciona</option>
-										<option value="">&lt;50% de las necesidades energéticas &gt;1 semana</option>
-										<option value="">Cualquier reducción durante &gt;2 semanas</option>
-										<option value="">Cualquier condición GI crónica que tenga un impacto adverso en la asmilación o absorción de alimentos</option>
-										<option value="">No aplica</option>
+										<option value="&lt;50% de las necesidades energéticas &gt;1 semana">&lt;50% de las necesidades energéticas &gt;1 semana</option>
+										<option value="Cualquier reducción durante &gt;2 semanas">Cualquier reducción durante &gt;2 semanas</option>
+										<option value="Cualquier condición GI crónica que tenga un impacto adverso en la asmilación o absorción de alimentos">Cualquier condición GI crónica que tenga un impacto adverso en la asmilación o absorción de alimentos</option>
+										<option value="No aplica">No aplica</option>
 									</select>
 								</div>
 
 								<div className="space-y-2">
 									<div className="">Inflamación:</div>
-									<select name="" id="" className="border border-brand-aqua w-full px-2 py-1 focus:outline-none">
+									<select 
+										name="inflammation" 
+										onChange={handleChange} 
+										className="border border-brand-aqua w-full px-2 py-1 focus:outline-none">
 										<option value="">Selecciona</option>
-										<option value="">Relacionada a enfermedad/lesión aguda</option>
-										<option value="">Relacionada a enfermedad/lesión crónica</option>
-										<option value="">No aplica</option>
+										<option value="Relacionada a enfermedad/lesión aguda">Relacionada a enfermedad/lesión aguda</option>
+										<option value="Relacionada a enfermedad/lesión crónica">Relacionada a enfermedad/lesión crónica</option>
+										<option value="No aplica">No aplica</option>
 									</select>
 								</div>
 							</section>
@@ -177,46 +362,10 @@ export default function GlimPage(){
 			
 			:
 
-			(<Results 
-				props={{
-					malnutrition,
-					footer_reset_btn: "NUEVA VALORACIÓN PRONTO",
-					formReset: () => { 
-						setSent(false); 
-						formReset(); 
-						setMalnutrition(null); 
-						setBreadcrumb('pronto'); 
-					}
-				}}>
-				<div className="text-center mt-3">Intervención nutricional recomendada:</div>
-
-				<div className="leading-tight space-y-6 mt-6">
-					<div className="flex gap-x-4">
-						<div className=""></div>
-						<div className="pl-4 relative">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-brand-dark w-2 left-0 top-2 absolute">
-								<path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"></path>
-							</svg>
-							<p>Es posible que su paciente necesite un tratamiento nutricional basándonos en las guías ESPEN/ESMO para pacientes con cáncer.</p>
-						</div>
-					</div>
-
-					<div className="flex gap-x-4">
-						<div className=""></div>
-						<div className="pl-4 relative">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-brand-dark w-2 left-0 top-2 absolute">
-								<path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"></path>
-							</svg>
-							<p>A continuación, desde Abbott Nutrition le facilitamos un algoritmo de decisión de nutrición enteral en paciente oncológico, donde podrá igualmente valorar los suplementos nutricionales valorar los suplementos nutricionales que mejor respondan a las necesidades de su paciente.</p>
-						</div>
-					</div>
-				</div>
-
-				<div className="mt-10 flex flex-col gap-4">
-					<div className="bg-brand-aqua text-white font-bold text-center leading-none w-full max-w-xs p-4 mx-auto select-none transition-all hover:opacity-80">ALGORITMO DE DECISIÓN DE NUTRICIÓN ENTERAL EN PACIENTE ONCOLÓGICO</div>
-					<Link href="/glim" className="bg-brand-aqua text-white font-bold text-center leading-none w-full max-w-xs p-4 mx-auto select-none transition-all hover:opacity-80">REALIZAR DIAGNÓSTICO GLIM</Link>
-				</div>
-			</Results>)
+			(
+			<>
+				<Results props={{diagnosis, ...form}}/>
+			</>)
 		}
 
 
