4e30bab07e71a1ff88fb9854379ad51334b50294
fitodac <fitodac@gmail.com>
2023-07-20 22:49:05 -0300

    create pdf component

--- src/components/CreatePdf.jsx
+++ src/components/CreatePdf.jsx
@@ -1,6 +1,7 @@
-export default function CreatePdf(){
+export default function CreatePdf(props){
 	return (<button 
-						className="bg-brand-dark text-white font-bold leading-none text-center w-full max-w-xs p-3 select-none transition-all hover:opacity-90">
+						className="bg-brand-dark text-white font-bold leading-none text-center w-full max-w-xs p-3 select-none transition-all hover:opacity-90"
+						onClick={() => props.generatePDF()}>
 						CREAR PDF
 					</button>)
 }
