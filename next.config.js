f7ae6251061db4628425f5a8b5493d054727348a
fitodac <fitodac@gmail.com>
2023-07-21 13:16:43 -0300

    config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		FOOTER_CODE: process.env.FOOTER_CODE
+		FOOTER_CODE: process.env.FOOTER_CODE,
+		LINK_2_PDF_DOCUMENT: process.env.LINK_2_PDF_DOCUMENT
 	}
 }
 
