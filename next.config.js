6cdb33e568c76b3fc7455a0b4d16e509ca775550
fitodac <fitodac@gmail.com>
2023-07-26 09:16:43 -0300

    Configuration for static deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'export',
+	distDir: 'dist',
+	exportStatic: true,
+	images: {
+		unoptimized: true
+	},
+
+
 	env: {
 		FOOTER_CODE: process.env.FOOTER_CODE,
 		LINK_2_PDF_DOCUMENT: process.env.LINK_2_PDF_DOCUMENT
-	}
+	},
 }
 
 module.exports = nextConfig
